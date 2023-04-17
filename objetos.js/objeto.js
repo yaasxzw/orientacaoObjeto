63eaa25820104caae098cfa6776c2ce6befc6663
Yasmim Guedes <110118206+yaasxzw@users.noreply.github.com>
2023-04-17 16:38:03 -0300

    Add files via upload

---
+++ objetos.js/objeto.js
@@ -0,0 +1,20 @@
+//Código não executável
+
+//Precedural
+processamento(valor1, valor2, valor3)
+
+//Orientação ao objeto
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){}
+}
+
+objeto = ['valor1', 'valor2', 'valor3']
+
+//Principios da orientação ao objeto
+//1- abstrção
+//2- encapsulamento
+//3- herança(prototype)
+//4- polimorfismo
